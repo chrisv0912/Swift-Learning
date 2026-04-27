1409152976432c7595450366dc13256a39e8d128
Christian Vera-Rojas <chrisv0912@outlook.com>
2026-04-27 15:55:51 -0400

    This is the first video I have watched, Swift Basics

---
+++ SwiftLearningPlayground.playground/Pages/SwiftBasics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+var greeting = "Hello, playground"
+
+print(greeting)
+//This is how you comment on Swift
+/*
+ This is how you write a multi-line comment
+ 
+ */
+
+//Naming Conventions
+let firstGreeting = "Hello world!"
+//We use camel case in Swift
+let thisIsMyFirstGreeting = "Hello world!"
+
+let thisismysecondgreeting = "Hello world!"
+//this is still usable, but is not the industry standard
+//makes it harder to read
